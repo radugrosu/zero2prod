66084fc59a27a10c8e3c00c9046b87a573b3b223
radu <radu.grosu@gmail.com>
2024-02-09 20:09:10 +0000

    use tcp listener in run

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,11 +1,13 @@
+use std::net::TcpListener;
+
 // You can inspect what code gets generated using
 // `cargo expand --test health_check` (<- name of the test file)
 #[tokio::test]
 async fn health_check_works() {
-    spawn_app();
+    let address = spawn_app();
     let client = reqwest::Client::new();
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(format!("{address}/health_check"))
         .send()
         .await
         .expect("Failed to execute request.");
@@ -13,10 +15,13 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 // Launch our application in the background
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
     // Launch the server as a background task
     // tokio::spawn returns a handle to the spawned future,
     // but we have no use for it here, hence the non-binding let
+    let server = zero2prod::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
