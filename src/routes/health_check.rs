451c4a0c6029ac598408dddf65a37053c7979fd2
radu <radu.grosu@gmail.com>
2024-02-11 14:42:54 +0000

    add connection as parameter to `startup.run`

--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -2,7 +2,30 @@ use std::net::TcpListener;
 
 use actix_web::{HttpResponse, Responder};
 
+use crate::configuration::get_configuration;
 use crate::startup;
+use sqlx::{Connection, PgConnection};
+
+async fn get_connection() -> PgConnection {
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.")
+}
+
+// Launch our application in the background
+#[allow(unused)]
+async fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    // Launch the server as a background task
+    // tokio::spawn returns a handle to the spawned future, but we have no use for it here
+    let connection = get_connection().await;
+    let server = startup::run(listener, connection).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
+}
 
 pub async fn health_check() -> impl Responder {
     HttpResponse::Ok()
@@ -11,46 +34,44 @@ pub async fn health_check() -> impl Responder {
 // `cargo expand --test health_check` (<- name of the test file)
 #[tokio::test]
 async fn health_check_works() {
-    let address = spawn_app();
+    let address = spawn_app().await;
     let client = reqwest::Client::new();
     let response = client
-        .get(format!("{address}/health_check"))
+        .get(format!("{}/health_check", address))
         .send()
         .await
         .expect("Failed to execute request.");
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
-// Launch our application in the background
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    let port = listener.local_addr().unwrap().port();
-    // Launch the server as a background task
-    // tokio::spawn returns a handle to the spawned future,
-    // but we have no use for it here, hence the non-binding let
-    let server = startup::run(listener).expect("Failed to bind address");
-    let _ = tokio::spawn(server);
-    format!("http://127.0.0.1:{}", port)
-}
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
-    let app_address = spawn_app();
+    let address = spawn_app().await;
+    // The `Connection` trait MUST be in scope for us to invoke
+    // `PgConnection::connect` - it is not an inherent method of the struct!
     let client = reqwest::Client::new();
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let response = client
-        .post(&format!("{}/subscriptions", &app_address))
+        .post(&format!("{}/subscriptions", &address))
         .header("Content-Type", "application/x-www-form-urlencoded")
         .body(body)
         .send()
         .await
         .expect("Failed to execute request.");
     assert_eq!(200, response.status().as_u16());
+    let mut connection = get_connection().await;
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
 async fn subscribe_returns_a_400_when_data_is_missing() {
-    let app_address = spawn_app();
+    let app_address = spawn_app().await;
     let client = reqwest::Client::new();
     let test_cases = vec![
         ("name=le%20guin", "missing the email"),
