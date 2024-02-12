c40bdafff2a45b556016edad8b6e337d7122428c
radu <radu.grosu@gmail.com>
2024-02-12 17:57:28 +0000

    manage request ids via tracing_actix_web

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -36,7 +36,10 @@ pub async fn insert_subscriber(
 #[tracing::instrument( 
     name = "Adding a new subscriber", 
     skip(form, pool),
-    fields( request_id = %Uuid::new_v4(), subscriber_email = %form.email, subscriber_name = %form.name )
+    fields(
+        subscriber_email = %form.email,
+        subscriber_name = %form.name 
+    )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     match insert_subscriber(&pool, &form).await {
