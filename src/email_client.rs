d74ebd383cf1fbaccfb08596be692d04cb86f8dc
radu <radu.grosu@gmail.com>
2024-02-16 21:10:43 +0000

    add default timeout to email client

--- src/email_client.rs
+++ src/email_client.rs
@@ -66,8 +66,12 @@ impl EmailClient {
         sender: SubscriberEmail,
         authorization_token: Secret<String>,
     ) -> Self {
+        let http_client = Client::builder()
+            .timeout(std::time::Duration::from_secs(10))
+            .build()
+            .unwrap();
         Self {
-            http_client: Client::new(),
+            http_client,
             base_url,
             sender,
             authorization_token,
@@ -88,6 +92,33 @@ mod tests {
     use wiremock::matchers::any;
     use wiremock::matchers::{header, header_exists, method, path};
     use wiremock::{Mock, MockServer, Request, ResponseTemplate};
+
+    #[tokio::test]
+    async fn send_email_time_out_if_server_takes_too_long() {
+        // Arrange
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(
+            Url::parse(&mock_server.uri()).unwrap(),
+            sender,
+            Secret::new(Faker.fake()),
+        );
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..10).fake();
+        Mock::given(any())
+            .respond_with(
+                ResponseTemplate::new(200).set_delay(std::time::Duration::from_secs(180)),
+            )
+            .expect(1)
+            .mount(&mock_server)
+            .await;
+        // Act
+        let outcome = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
+        assert_err!(outcome);
+    }
 
     #[tokio::test]
     async fn send_email_succeeds_if_the_server_returns_200() {
